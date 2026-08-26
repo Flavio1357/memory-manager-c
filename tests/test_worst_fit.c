9ad15282a98b26f8aeedfe4358e863c45b3916a8
Flavio Filho <fpneu911@gmail.com>
2026-08-26 13:48:53 -0300

    test do algoritmo worst fit

---
+++ tests/test_worst_fit.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+
+#include "memory.h"
+#include "block.h"
+#include "allocator.h"
+
+int main(void) {
+
+    Block *memory = memory_init();
+
+    if (memory == NULL) {
+        printf("Erro ao inicializar a memoria.\n");
+        return 1;
+    }
+
+    /*
+     * Criando:
+     *
+     * [100 LIVRE][500 USADO][200 LIVRE][224 LIVRE]
+     */
+
+    allocate(memory, 100, FIRST_FIT);
+    allocate(memory, 500, FIRST_FIT);
+    allocate(memory, 200, FIRST_FIT);
+
+    free_memory(memory, 0);
+    free_memory(memory, 600);
+
+    printf("=================================\n");
+    printf("         TESTE WORST FIT\n");
+    printf("=================================\n\n");
+
+    printf("Memoria antes da alocacao:\n");
+    block_print_all(memory);
+
+    printf("\nAlocando 150 bytes com WORST FIT...\n\n");
+
+    Block *allocated = allocate(memory, 150, WORST_FIT);
+
+    if (allocated != NULL) {
+        printf("150 bytes alocados com sucesso!\n");
+        printf("Bloco escolhido: inicio = %zu\n", allocated->start);
+    } else {
+        printf("Falha na alocacao.\n");
+    }
+
+    printf("\nMemoria depois da alocacao:\n");
+    block_print_all(memory);
+
+    memory_destroy(memory);
+
+    return 0;
+}
