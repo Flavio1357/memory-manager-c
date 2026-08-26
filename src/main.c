271e964d76e4f21e49034531a3d799ebe0079575
Flavio Filho <fpneu911@gmail.com>
2026-08-25 23:36:08 -0300

    Criação da Main e do include Block

---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+#define MEMORY_SIZE 1024
+
+int main() {
+
+    printf("=================================\n");
+    printf("        MEMORY MANAGER\n");
+    printf("=================================\n");
+
+    printf("Memoria total: %d bytes\n", MEMORY_SIZE);
+
+    return 0;
+}
